13c65305e73559ab3f8675565f5a86662223f02b
Jim Kaya <Jim.kaya@gmail.com>
2022-05-25 11:56:17 +0200

    added hooks

--- src/view/HomeView/HomeView.js
+++ src/view/HomeView/HomeView.js
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+
 export const HomeView = () => {
+  const [count, setCount] = useState(1); // count = lagrar värdet. setCount = uppdaterer det lagrade värdet.
+
+  useEffect(() => {
+    alert("Welcome!"); // När sidan laddas första gången.
+    return () => {
+      alert("Goodbye!"); // När vi lämnar sidan.
+    };
+  }, []); // Säkerställer att vi bara kör funktionen en gång.
+
+  useEffect(() => {
+    alert("Welcome!"); // När sidan laddas första gången.
+    return () => {
+      alert("Goodbye!"); // När vi lämnar sidan.
+    };
+  }, [count]); // Triggas när count uppdateras.
+
   return (
     <div>
       <h1>HomeView</h1>
+      {/* <h2>Count is: {count}</h2>
+      <button onClick={() => setCount(5)}>5</button> Ändrar count till 5 */}
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)} disabled={count === 0}>
+        -
+      </button>
     </div>
   );
 };
